e9fdeadaa9d1f25785c51277d89cca2f83cde29c
aishanaeem644 <151793460+aishanaeem644@users.noreply.github.com>
2024-09-16 22:44:02 -0600

    week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Aisha Naeem</h1>
+      <p>
+        <Link href="https://github.com/aishanaeem644" passHref>github.com</Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
