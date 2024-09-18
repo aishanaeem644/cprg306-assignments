a530b455afb3e95b6810deb8865e7c3ec6151a2b
aishanaeem644 <151793460+aishanaeem644@users.noreply.github.com>
2024-09-18 09:52:03 -0600

    webdev 2 assignment 2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 const StudentInfo = () => {
   return (
     <div>
       <h1>Aisha Naeem</h1>
       <p>
-        <Link href="https://github.com/aishanaeem644" passHref>github.com</Link>
+        <Link href="https://github.com/aishanaeem644">http://github.com</Link>
       </p>
     </div>
   );
